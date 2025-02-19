aaeea7671bf63ccc94ea0b9151b8028c6c60b54c
akshath7135 <akshathabrol21122004@gmail.com>
2025-02-19 19:28:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+  const frequency = {};
+
+  for (let i = 0; i < str.length; i++) {
+    frequency[str[i]] = (frequency[str[i]] || 0) + 1;
+  }
+  for (let i = 0; i < str.length; i++) {
+    if (frequency[str[i]] === 1) {
+      return str[i];  
+    }
+  }
+  return null;
+}
+
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
